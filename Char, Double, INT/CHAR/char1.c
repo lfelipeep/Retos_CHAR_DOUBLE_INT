72fec255267237fb12d1252581b4615fef53fec9
lfelipeep <luisfecardozoherrada@gmail.com>
2025-08-30 19:09:44 -0500

    Update char1.c

--- Char, Double, INT/CHAR/char1.c
+++ Char, Double, INT/CHAR/char1.c
@@ -6,4 +6,13 @@ int main() {
     scanf(" %c", &c);
     printf("El codigo ASCII de '%c' es: %d\n", c, c);
     return 0;
+
 }
+
+## Sección 1: char en C
+
+El tipo de dato `char` se utiliza para almacenar un solo carácter, como una letra, número o símbolo.
+
+### 1. Mostrar el código ASCII de un carácter
+
+Lee un carácter y muestra su código ASCII.
